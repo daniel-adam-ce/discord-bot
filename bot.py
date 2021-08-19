24129a06b42fa9f89b60c37c267d5466168861bc
Daniel Adam <danieladamce@gmail.com>
2021-08-18 22:09:43 -0700

    Added error catching
    Load, unload, and reload commands now will send error to user using command if there is an error.

--- bot.py
+++ bot.py
@@ -13,32 +13,41 @@ client = commands.Bot(command_prefix = ".")
 
 @client.command()
 async def load(ctx,extension):
-    if ctx.author.guild_permissions.manage_roles:
-        client.load_extension(f'cogs.{extension}')
-        await ctx.message.add_reaction('\u2705')
-    else:
-        await ctx.message.add_reaction('\u274c')
+    try:
+        if ctx.author.guild_permissions.manage_roles:
+            client.load_extension(f'cogs.{extension}')
+            await ctx.message.add_reaction('\u2705')
+        else:
+            await ctx.message.add_reaction('\u274c')
+    except Exception as ex:
+        await ctx.send(f'Something went wrong: {ex}')
 
 @client.command()
 async def unload(ctx,extension):
-    if ctx.author.guild_permissions.manage_roles:
-        client.unload_extension(f'cogs.{extension}')
-        await ctx.message.add_reaction('\u2705')
-    else:
-        await ctx.message.add_reaction('\u274c')
+    try:
+        if ctx.author.guild_permissions.manage_roles:
+            client.unload_extension(f'cogs.{extension}')
+            await ctx.message.add_reaction('\u2705')
+        else:
+            await ctx.message.add_reaction('\u274c')
+    except Exception as ex:
+        await ctx.send(f'Something went wrong: {ex}')
     
 @client.command()
 async def reload(ctx, extension):
-    if ctx.author.guild_permissions.manage_roles:
-        client.unload_extension(f'cogs.{extension}')
-        await ctx.message.add_reaction('\u2705')
-    else:
-        await ctx.message.add_reaction('\u274c')
-    if ctx.author.guild_permissions.manage_roles:
-        client.load_extension(f'cogs.{extension}')
-        await ctx.message.add_reaction('\u2705')
-    else:
-        await ctx.message.add_reaction('\u274c')
+    try:
+        if ctx.author.guild_permissions.manage_roles:
+            client.unload_extension(f'cogs.{extension}')
+            await ctx.message.add_reaction('\u2705')
+        else:
+            await ctx.message.add_reaction('\u274c')
+        if ctx.author.guild_permissions.manage_roles:
+            client.load_extension(f'cogs.{extension}')
+            await ctx.message.add_reaction('\u2611')
+        else:
+            await ctx.message.add_reaction('\u2716')
+    except Exception as ex:
+        await ctx.send(f'Something went wrong: {ex}')
 
 
 
@@ -52,9 +61,6 @@ token = data['token']
 
 
 slash = SlashCommand(client, sync_commands=True)
-# @slash.slash(name="test", description="This is just a test command, nothing more.", guild_ids=[203376646319767553])
-# async def test(ctx):
-#   await ctx.send(content="Hello World!")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
